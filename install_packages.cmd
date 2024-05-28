636fc2fc35384907c55f4bbf45b84a6b2e0f7eb3
Austin Lackey <austinblackey@gmail.com>
2024-05-28 15:34:16 -0600

    Update

---
+++ install_packages.cmd
@@ -0,0 +1,4 @@
+pip3 install -U --trusted-host pypi.org --trusted-host files.pythonhosted.org openpyxl
+pip3 install -U --trusted-host pypi.org --trusted-host files.pythonhosted.org numpy
+pip3 install -U --trusted-host pypi.org --trusted-host files.pythonhosted.org pandas
+pip3 install -U --trusted-host pypi.org --trusted-host files.pythonhosted.org tqdm
